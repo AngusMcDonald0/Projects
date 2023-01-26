af06fb7befd4ee1f8775adcb11fa393fb35deaba
Angus McDonald <angusmcdonald13@gmail.com>
2023-01-26 17:48:36 +1100

    psuedo code

--- cypher/Program.cs
+++ cypher/Program.cs
@@ -1,6 +1,23 @@
 
 // input string
+
 // convert string from ascii to hexidecimal
 
+// encrypt hexidecimal with key
 
-// encrypt hexidecimal 
+// output encrypted response
+
+
+
+
+
+
+
+
+// input encrypted message
+
+// decrypt with key
+
+// convert back to readable text
+
+// output decoded message
